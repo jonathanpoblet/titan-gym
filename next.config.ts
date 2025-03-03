9293f6a93be8eab9e83641026ea4bbeeee6dabb9
Jonathan Poblet <jonathanpoblet228@gmail.com>
2025-03-03 10:36:27 -0300

    Proyect Initialized

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+};
+
+export default nextConfig;
